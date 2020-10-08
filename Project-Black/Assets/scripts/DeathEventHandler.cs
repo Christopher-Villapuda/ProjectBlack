a55a1475ed63c7f211da043d170f5be36f956c18
Christopher-Villapuda <49170925+Christopher-Villapuda@users.noreply.github.com>
2020-10-08 18:29:46 -0500

    Added Death Handler Script
    Script that kind of works to create a game end.

---
+++ Project-Black/Assets/scripts/DeathEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Fungus;
+using System.Collections.Generic;
+using UnityEngine;
+
+[EventHandlerInfo("Test", "Death","This is a test tooltip.")] 
+
+public class DeathEventHandler : EventHandler
+{
+    private void OnPlayerDied()
+    {
+        ExecuteBlock();
+    }
+    private void OnEnable()
+    {
+        PlayerHealth.PlayerDied += OnPlayerDied;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.PlayerDied -= OnPlayerDied;
+    }
+}
---
+++ Project-Black/Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static event Action PlayerDied;
+
+    [SerializeField]
+    private int maxLives = 3;
+    private int currentLives;
+    private void Start()
+    {
+        currentLives = maxLives;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentLives -= amount;
+        DeathCheck();
+    }
+
+    private void DeathCheck()
+    {
+       if(currentLives <= 0)
+        {
+            PlayerDied?.Invoke();
+        }
+    }
+}
+
